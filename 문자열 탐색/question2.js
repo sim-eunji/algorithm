033585d772741a652223849d96dfa5a9eb440890
sim-eunji <kingeunji12@gmail.com>
2021-02-13 23:59:27 +0900

    자료구조 개요 Readme.md 추가

---
+++ 문자열 탐색/question2.js
@@ -0,0 +1,12 @@
+function solution(s) {
+  let answer = "YES";
+  s = s.toLowerCase().replace(/[^a-z]/g, '')
+  for (let i = 0; i < Math.floor(s.length / 2); i++) {
+    if (s[i] != s[(s.length - 1) - i]) answer = "NO"
+  }
+
+  return answer;
+}
+
+let str = "found7, time: study; Yduts; emit, 7Dnuof";
+console.log(solution(str)); 
